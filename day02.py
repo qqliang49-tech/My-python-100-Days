ac6a66e2f9fa4b9ea1c1ddbbaabd22e22262d557
qqliang49-tech <qqliang49@gmail.com>
2026-02-20 19:44:14 +0800

    Update day02.py

--- day02.py
+++ day02.py
@@ -1,13 +1,14 @@
 video_titles =["云边有个小卖部","我与地坛","偷影子的人"]
-new_titles = input("请输入新的视频标题：")
+new_titles = new_titles = input("请输入新的视频标题：")input      输入输入("请输入新的视频标题：")
 video_titles.append(new_titles)
-fans_count = int(input("请输入粉丝数量："))
-likes_count = int(input("请输入点赞数量："))
-ratio = likes_count / fans_count
-ratio = round(ratio, 2)
-print("---账号数据简报---")
-print(f"当前共有视频标题；{video_titles}")
-print(f"视频总数：{len(video_titles)}个")
-print(f"账号权重分析；您的赞粉比为{ratio}")
+fans_count = fans_count = int(input   输入("请输入粉丝数量："))int(input   输入("请输入粉丝数量："))
+likes_count = likes_count = int(input   输入("请输入点赞数量："))int(input   输入("请输入点赞数量："))
+ratio = likes_count / fans_countRatio = like_count / fans_count
+ratio =    Ratio = round（Ratio, 2）round(ratio, 2)
+print   打印   打印("---账号数据简报---")
+printprint   打印(f"当前共有视频标题：{video_titles}")(f"当前共有视频标题；{video_titles}")
+printprint   打印(f"视频总数：{len(video_titles)}个")(f"视频总数：{len(video_titles)}个")
+print   打印   打印(f"账号权重分析；您的赞粉比为{ratio}")
 
-print("---状态：准备开启AI自动化---")
+print   打印   打印("---状态：准备开启AI自动化---")
+
